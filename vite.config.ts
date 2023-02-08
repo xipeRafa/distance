bb3a4b56dfc6a1fe4eb6a925db4e4ed2a044bf95
fer <fer@fers-iMac.local>
2023-02-07 22:46:10 -0700

    okk

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,28 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      './runtimeConfig': './runtimeConfig.browser',
+    },
+  },
   plugins: [react()],
 })
+
+
+
+
+/* import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      './runtimeConfig': './runtimeConfig.browser',
+    },
+  },
+  plugins: [
+    react()
+  ]
+}) */
